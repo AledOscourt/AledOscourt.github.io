95ab5b01d551f3de9566f117ebaec77e9d4416de
AledOscourt <julien.fournier2508@gmail.com>
2021-12-04 23:28:03 +0100

    header fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
+AOS.init();
 let navbar = document.getElementById('navbar');
 let navbarNeon = document.getElementById('navbarNeon')
 window.onscroll = () => {
     if (window.scrollY >= 10) {
-        navbar.style.backgroundColor = 'rgba(0,0,0,0.7)';
+        navbar.style.backgroundColor = 'rgba(0,0,0,0.8)';
         navbar.style.boxShadow = '0px 0px 2px #FFFAFA, 0px 0px 5px #e840ba, 0px 0px 15px 2px #FFFAFA, 0px 0px 20px 5px #e840ba';
     } else if (window.innerWidth > 756) {
         navbar.style.backgroundColor = 'rgba(0,0,0,0.2)';
@@ -14,7 +15,7 @@ window.onscroll = () => {
 }
 window.onload = () => {
     let typed = new Typed('#formationHeader', {
-        strings: [, 'DÉVELOPPEUR WEB ...^1500', 'ET WEB MOBILE JUNIOR^1500'],
+        strings: [, 'DÉVELOPPEUR WEB ...^1500', 'ET WEB MOBILE^1500'],
         typeSpeed: 80,
         backSpeed: 90,
         showCursor: false,
