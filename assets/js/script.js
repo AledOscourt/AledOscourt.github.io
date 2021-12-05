a4f3be848261c84fc7084aef440e45eda128dd6e
AledOscourt <julien.fournier2508@gmail.com>
2021-12-05 15:59:22 +0100

    scrool start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
+/****************************************************************************************
+ **************************************** AOS INIT **************************************
+ ***************************************************************************************/
 AOS.init();
+/****************************************************************************************
+ **************************************** Variable **************************************
+ ***************************************************************************************/
 let navbar = document.getElementById('navbar');
-let navbarNeon = document.getElementById('navbarNeon')
+let navbarNeon = document.getElementById('navbarNeon');
+let homeOfPageNavbar = document.getElementById('homeOfPageNavbar');
+let aboutMeNavbar = document.getElementById('aboutMeNavbar');
+let skillNavbar = document.getElementById('skillNavbar');
+
+//Empéche la sélection de texte dans le document ainsi que le dblclick
+document.addEventListener('mousedown', function(e) { e.preventDefault(); }, false);
+/****************************************************************************************
+ **************************************** Navbar ****************************************
+ ***************************************************************************************/
 window.onscroll = () => {
     if (window.scrollY >= 10) {
         navbar.style.backgroundColor = 'rgba(0,0,0,0.8)';
-        navbar.style.boxShadow = '0px 0px 2px #FFFAFA, 0px 0px 5px #e840ba, 0px 0px 15px 2px #FFFAFA, 0px 0px 20px 5px #e840ba';
+        navbar.style.boxShadow = '0px 0px 2px #FFFAFA, 0px 0px 5px #e840ba, 0px 0px 12px #020402, 0px 0px 15px #e840ba, 0px 0px 18px #020402, 0px 0px 20px #e840ba';
     } else if (window.innerWidth > 756) {
         navbar.style.backgroundColor = 'rgba(0,0,0,0.2)';
         navbar.style.boxShadow = 'none';
@@ -12,13 +27,29 @@ window.onscroll = () => {
         navbar.style.backgroundColor = 'rgba(0,0,0,0.5)';
         navbar.style.boxShadow = 'none';
     }
+
 }
 window.onload = () => {
     let typed = new Typed('#formationHeader', {
-        strings: [, 'DÉVELOPPEUR WEB ...^1500', 'ET WEB MOBILE^1500'],
+        strings: [, 'DÉVELOPPEUR WEB ...^1500', 'FULL STACK JUNIOR^1500'],
         typeSpeed: 80,
         backSpeed: 90,
         showCursor: false,
         loop: true,
     });
+}
+
+/****************************************************************************************
+ **************************************** Navbar nav onclick ****************************
+ ***************************************************************************************/
+homeOfPageNavbar.onclick = () => {
+    window.scrollTo(0, 0);
+
+}
+
+aboutMeNavbar.onclick = () => {
+    window.scrollTo(0, 720);
+}
+skillNavbar.onclick = () => {
+    window.scrollTo(0, 1220);
 }
